78cc17959fa2206f0f2cab0feaa97664faef0f86
Scott Cox <scottcox89@gmail.com>
2019-01-30 18:43:12 +0000

    Change createThreeJSDefaults() - Remove scene.add(light) - Remove scene.add(camera) - Change light to ambientLight - Add stats

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,23 +1,23 @@
 import * as Three from "three";
+import Stats from "stats.js";
 
 export const createThreeJSDefaults = () => {
   const { innerWidth, innerHeight, devicePixelRatio } = window;
   const scene = new Three.Scene();
   const renderer = window.WebGLRenderingContext ? new Three.WebGLRenderer() : new Three.CanvasRenderer();
   const camera = new Three.PerspectiveCamera(35, innerWidth / innerHeight, 1, 1000);
-  const light = new Three.AmbientLight(0xffffff);
+  const ambientLight = new Three.AmbientLight(0xffffff);
+  const stats = new Stats();
 
   renderer.setSize(innerWidth, innerHeight);
   renderer.setPixelRatio(devicePixelRatio);
   renderer.setClearColor(0xc5c5c3);
 
-  scene.add(light);
-  scene.add(camera);
-
   return {
     scene,
     renderer,
     camera,
-    light,
+    ambientLight,
+    stats,
   }
 };
