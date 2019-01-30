03ba3ef12971b926f99d4ceb5ad1df36d29b8728
Scott Cox <scottcox89@gmail.com>
2019-01-30 12:59:08 +0000

    Add react-router routing to <App/> to allow component selection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+
+import Cube from "./components/Cube";
+import Line from "./components/Line";
 import Model from "./components/Model";
+
+const Index = () => (
+  <div>Choose an example from the nav menu</div>
+);
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Model/>
-      </div>
+      <Router>
+        <div>
+          <header>
+            <nav>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/cube">Cube</Link></li>
+                <li><Link to="/line">Line</Link></li>
+                <li><Link to="/model">Model</Link></li>
+              </ul>
+            </nav>
+          </header>
+          <main id="main">
+            <Route path="/" exact component={Index} />
+            <Route path="/cube" component={Cube} />
+            <Route path="/line" component={Line} />
+            <Route path="/model" component={Model} />
+          </main>
+        </div>
+      </Router>
     )
   }
 }
